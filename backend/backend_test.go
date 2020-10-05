a588f03fe37856e8e7eba3e47e3b8672ff1a70be
Reed Allman <reed@rdallman.com>
2020-10-05 12:50:04 -0700

    passing totals test

--- backend/backend_test.go
+++ backend/backend_test.go
@@ -66,6 +66,7 @@ func TestTokenBuckets(t *testing.T) {
 		err error
 		exp []*models.TokenBucket
 	}{
+		{"0x0", start.Add(-1 * time.Hour), start.Add(-1 * time.Minute), 0, nil, seed[:0]},
 		{"0x0", start, start.Add(59 * time.Minute), 0, nil, seed[:1]},
 		{"0x0", start, start.Add(61 * time.Minute), 0, nil, seed[:2]},
 		{"0x1", start, start.Add(24 * time.Hour), 0, nil, seed[2:3]},
@@ -73,6 +74,72 @@ func TestTokenBuckets(t *testing.T) {
 
 	for i, test := range tests {
 		tb, err := db.GetTokenBuckets(ctx, test.addr, test.from, test.to, test.frame)
+		if err != test.err {
+			t.Errorf("test %v | error mismatch:\nexpected: %v\ngot: %v", i, test.err, err)
+			continue
+		}
+
+		if !reflect.DeepEqual(tb, test.exp) {
+			t.Errorf("test %v | results mismatch:\nexpected: %v\ngot: %v", i, test.exp, tb)
+		}
+	}
+}
+
+func TestTotalBuckets(t *testing.T) {
+	start := time.Now()
+
+	//zero := decimal.NewFromFloat(0)
+	one := decimal.NewFromFloat(1)
+	two := decimal.NewFromFloat(2)
+	//three := decimal.NewFromFloat(3)
+	four := decimal.NewFromFloat(4)
+
+	seed := []*models.TotalBucket{
+		{
+			Time:         start,
+			VolumeUSD:    one,
+			LiquidityUSD: one,
+		},
+		{
+			Time:         start.Add(1 * time.Hour),
+			VolumeUSD:    one,
+			LiquidityUSD: two,
+		},
+		{
+			Time:         start.Add(2 * time.Hour),
+			VolumeUSD:    four,
+			LiquidityUSD: two,
+		},
+	}
+
+	day := []*models.TotalBucket{
+		{
+			Time:         start,
+			VolumeUSD:    two.Add(four),
+			LiquidityUSD: two,
+		},
+	}
+
+	ctx := context.Background()
+	db := NewMock(seed)
+
+	tests := []struct {
+		from, to time.Time
+		frame    time.Duration
+
+		err error
+		exp []*models.TotalBucket
+	}{
+		{start.Add(-1 * time.Hour), start.Add(-1 * time.Minute), 0, nil, nil},
+		{start, start.Add(59 * time.Minute), 0, nil, seed[:1]},
+		{start, start.Add(59 * time.Minute), 1 * time.Hour, nil, seed[:1]},
+		{start, start.Add(61 * time.Minute), 1 * time.Hour, nil, seed[:2]},
+		{start, start.Add(3 * time.Hour), 1 * time.Hour, nil, seed[:]},
+		{start, start.Add(3 * time.Hour), 24 * time.Hour, nil, day},
+	}
+
+	for i, test := range tests {
+		tb, err := db.GetTotals(ctx, test.from, test.to, test.frame)
 		if err != test.err {
 			t.Errorf("test %v | error mismatch:\nexpected: %v\ngot: %v", i, test.err, err)
 			continue
